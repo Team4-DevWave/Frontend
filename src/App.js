67c21cb839db6fc9c5a70fe93a5685597755e028
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2024-03-04 23:17:48 +0200

    Created basic login-signup layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Route path="/" element={<Navigate to="/home" />} />
       <Route path="/home" element={<div>Home</div>} />
       <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<div>Contact</div>} />
+      <Route path="/signup" element={<Signup/>} />
       <Route path="/settings" element={<Settings/>} />
     </Routes>
     </BrowserRouter>
