f41798e149a1d7322a27098e5965e3a3660842ba
Tarek Emad <tarek.mohamed04@eng-st.cu.edu.eg>
2024-03-07 22:37:46 +0200

    messages is done but style need more modifications

---
+++ src/components/messages/MessageNav.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Tabs, Tab } from '@mui/material';
+import SendMessage from "./SendMessages";
+import Inbox from './inboxcomponent';
+import SentMessage from './SentMessage';
+import MentionedUsername from './MentionedUsername';
+import Notifications from "./Notifications";
+// import Safety from './Safety';
+// import Feed from './Feed';
+// import Notifications from './Notifications';
+// import Emails from './Emails';
+// import Subscriptions from './Subscriptions';
+// import Chat from './Chat';
+
+function MessagesNav() {
+    const [activeNavItem, setActiveNavItem] = useState(0);
+
+    const handleTabChange = (event, newValue) => {
+        setActiveNavItem(newValue);
+    };
+
+    return (
+        <>
+            <Tabs
+                className="navList sizeLg"
+                value={activeNavItem}
+                onChange={handleTabChange}
+                indicatorColor="primary"
+                textColor="primary"
+                left
+            >
+                <Tab label="Send Message" sx={{ textTransform: 'none', fontWeight:'bold', fontSize: 'var(--font-medium)','&:hover': {color: 'var(--color-black)',} }} />
+                <Tab label="Inbox" sx={{ textTransform: 'none', fontWeight:'bold', fontSize: 'var(--font-medium)','&:hover': {color: 'var(--color-black)',} }} />
+                <Tab label="sent" sx={{ textTransform: 'none', fontWeight:'bold', fontSize: 'var(--font-medium)','&:hover': {color: 'var(--color-black)',} }} />
+                <Tab label="Mentioned Username" sx={{ textTransform: 'none', fontWeight:'bold', fontSize: 'var(--font-medium)','&:hover': {color: 'var(--color-black)',} }} />
+                <Tab label="Notifications" sx={{ textTransform: 'none', fontWeight:'bold', fontSize: 'var(--font-medium)','&:hover': {color: 'var(--color-black)',} }} />
+
+            </Tabs>
+            <div class="horizontalLine"></div>
+
+            {activeNavItem === 0 && <SendMessage />}
+            {activeNavItem === 1 && <Inbox />}
+            {activeNavItem === 2 && <SentMessage />}
+            {activeNavItem === 3 && <MentionedUsername/>}
+            {activeNavItem === 4 && <Notifications />}
+            {/*{activeNavItem === 5 && <Safety />}*/}
+            {/*{activeNavItem === 3 && <Feed />}*/}
+            {/*{activeNavItem === 4 && <Notifications />}*/}
+            {/*{activeNavItem === 5 && <Emails />}*/}
+            {/*{activeNavItem === 6 && <Subscriptions />}*/}
+            {/*{activeNavItem === 7 && <Chat />}*/}
+        </>
+    );
+
+}
+
+export default MessagesNav;
---
+++ src/components/messages/inboxcomponent.js
@@ -0,0 +1,12 @@
+function inbox() {
+    return (
+        <div >
+            <div className="header">
+                <h1 className='title'>No message are received yet</h1>
+                <div class="horizontalLine"></div>
+            </div>
+        </div>
+    );
+}
+
+export default inbox;
