0035754471e200174596b72ee73fd5f3402ea7a5
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2024-03-20 10:12:06 +0200

    Fixed social logins

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId="500020411396-l7soq48qpasrds9ipgo5nff5656i0ial.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 reportWebVitals();
