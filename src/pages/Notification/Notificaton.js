eb9ab4ef4ebc698df40033ae0e30d8274a9f3cde
Tarek Emad <tarek.mohamed04@eng-st.cu.edu.eg>
2024-04-08 20:05:46 +0200

    notification integration with backend is done

--- src/pages/Notification/Notificaton.js
+++ src/pages/Notification/Notificaton.js
@@ -1,6 +1,7 @@
 import "./notification.css";
 import Header from "../../components/Header";
 import NotificationNav from "../../components/Notification/NotifactionNav";
+import {Meta} from '@storybook/react';
 function notification() {
   return (
     <div className="navbar-padding">
