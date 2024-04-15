f1985d73e3328bed1f6df3811c8af9a1be4ed25f
Ismostafa <103459574+Ismostafa@users.noreply.github.com>
2024-04-16 01:32:46 +0200

    profileNav

--- src/components/userProfile/ProfileNav.js
+++ src/components/userProfile/ProfileNav.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Tabs, Tab, Typography, useMediaQuery, useTheme } from "@mui/material";
 import axios from "axios";
-
+import UserPost from "../UserTabs/UserPost";
+import UserSavedPost from "../UserTabs/UserSavedPost";
+import UserHidden from "../UserTabs/UserHiddenPost";
 function ProfileNav({
   overviewData,
   postsData,
@@ -142,12 +144,21 @@ function ProfileNav({
       <div>
         {tabData ? (
           <Typography component="div" sx={{ p: 3 }}>
+<<<<<<< Updated upstream
             {activeTab === 0 && JSON.stringify(overviewData)}
             {activeTab === 1 && JSON.stringify(postsData)}
             {activeTab === 2 && JSON.stringify(commentsData)}
             {activeTab === 3 && JSON.stringify(savedData)}
             {activeTab === 4 && JSON.stringify(hiddenData)}
             {activeTab === 5 && JSON.stringify(upvotedData)}
+=======
+            {activeTab === 0 && <OverView  />}
+            {activeTab === 1 && <UserPost  />}
+            {activeTab === 2 && <UserComments />}
+            {activeTab === 3 && <UserSavedPost />}
+            {activeTab === 4 && <UserHidden/>}
+            {activeTab === 5 && <Upvoted />}
+>>>>>>> Stashed changes
             {activeTab === 6 && JSON.stringify(downvotedData)}
           </Typography>
         ) : (
