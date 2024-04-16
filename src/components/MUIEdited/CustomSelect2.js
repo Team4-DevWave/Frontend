ae6f7408d7768030983b674cd2fa10cb2a937912
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-04-16 17:05:59 +0200

    settings integrated with backend

--- src/components/MUIEdited/CustomSelect2.js
+++ src/components/MUIEdited/CustomSelect2.js
@@ -2,24 +2,19 @@ import React from 'react';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
-function CustomSelect({ defaultValue, values, mr, ml, onSelection }) {
-  const handleSelection = (event) => {
-    const selectedValue = event.target.value;
-    onSelection(selectedValue);
-  };
-
+function CustomSelect({ defaultValue, values,  mr, ml, onSelection }) {
   return (
     <>
       <Select
-        value={defaultValue} // Change defaultValue to value
-        onChange={handleSelection} // Use onChange instead of onClick
+        defaultValue={defaultValue}
+        onClick={onSelection}
         sx={{
           color: "var(--color-blue)",
           fontWeight: "bold",
           fontSize: "var(--font-medium)",
           '@media screen and (max-width: 1000px)': {
-            fontSize: 'var(--font-very-small)' // Font size for screens <1000px
-          },
+          fontSize: 'var(--font-very-small)' // Font size for screens <1000px
+        },
           marginRight: mr === 'none' ? 'none' : mr,
           marginLeft: ml === 'auto' ? 'auto' : ml,
         }}
@@ -31,7 +26,7 @@ function CustomSelect({ defaultValue, values, mr, ml, onSelection }) {
               color: "var(--color-gray)",
               fontSize: "var(--font-small)",
               '@media screen and (max-width: 1000px)': {
-                fontSize: 'var(--font-very-small)' // Font size for screens <1000px
+              fontSize: 'var(--font-very-small)' // Font size for screens <1000px
               },
             }}
             value={value}
