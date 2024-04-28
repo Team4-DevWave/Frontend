6e590890348d18566bdda08088e8456a9a0863e5
mohamed07abdelaziz <88625570+mohamed07abdelaziz@users.noreply.github.com>
2024-04-28 22:00:07 +0300

    headerModification

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -28,6 +29,8 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import TagIcon from "@mui/icons-material/Tag";
 import GestureIcon from "@mui/icons-material/Gesture";
+import Chat from "../components/Chat/ChatWindow.js";
+
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -53,6 +56,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   alignItems: "center",
   justifyContent: "center",
 }));
+
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
@@ -83,6 +87,8 @@ const SearchResults = styled("div")(({ theme }) => ({
 }));
 
 export default function Header() {
+  const [showChat, setShowChat] = useState(false);
+
   const [isSearchActive, setIsSearchActive] = React.useState(false);
   const [search, setSearch] = React.useState("");
   const matches = useMediaQuery("(max-width:1350px)");
@@ -250,12 +256,54 @@ export default function Header() {
         <p>Messages</p>
       </MenuItem>
       <MenuItem>
-      <Badge badgeContent={17} color="error">
+        <Badge badgeContent={17} color="error">
           <NotificationsIcon />
         </Badge>
 
         <p>Notifications</p>
+        
+
+
+
       </MenuItem>
+
+      <MenuItem>
+          {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+
+          <IconButton onClick={() => { setShowChat(true) }}
+            size="large"
+            aria-label="show 4 new mails"
+            color="inherit"
+          >
+
+
+
+            <Badge badgeContent={1} color="error">
+              <svg
+
+                rpl=""
+                fill="currentColor"
+                height="20"
+                icon-name="chat-outline"
+                viewBox="0 0 20 20"
+                width="20"
+                xmlns="http://www.w3.org/2000/svg"
+                role="svg"
+              >
+                <path d="M11.61 19.872a10.013 10.013 0 0 0 6.51-4.035A9.999 9.999 0 0 0 12.275.264c-1.28-.3-2.606-.345-3.903-.132a10.05 10.05 0 0 0-8.25 8.311 9.877 9.877 0 0 0 1.202 6.491l-1.24 4.078a.727.727 0 0 0 .178.721.72.72 0 0 0 .72.19l4.17-1.193A9.87 9.87 0 0 0 9.998 20c.54 0 1.079-.043 1.612-.128ZM1.558 18.458l1.118-3.69-.145-.24A8.647 8.647 0 0 1 1.36 8.634a8.778 8.778 0 0 1 7.21-7.27 8.765 8.765 0 0 1 8.916 3.995 8.748 8.748 0 0 1-2.849 12.09 8.763 8.763 0 0 1-3.22 1.188 8.68 8.68 0 0 1-5.862-1.118l-.232-.138-3.764 1.076ZM6.006 9a1.001 1.001 0 0 0-.708 1.707A1 1 0 1 0 6.006 9Zm4.002 0a1.001 1.001 0 0 0-.195 1.981 1 1 0 1 0 .195-1.98Zm4.003 0a1.001 1.001 0 1 0 0 2.003 1.001 1.001 0 0 0 0-2.003Z"></path>
+              </svg>
+            </Badge>
+
+            {showChat && <Chat />}
+            
+            <p>Chat</p>
+
+
+          </IconButton>
+
+          {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+        </MenuItem>
+
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
           size="large"
@@ -306,7 +354,7 @@ export default function Header() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-       <AppBar
+      <AppBar
         position="fixed"
         sx={{
           backgroundColor: "white",
@@ -367,8 +415,8 @@ export default function Header() {
               width: !searchSize ? "50%" : "",
             }}
           >
-             <SearchIconWrapper>
-             <SearchIcon onClick={() => navigate(`/search/${search}`)} />
+            <SearchIconWrapper>
+              <SearchIcon onClick={() => navigate(`/search/${search}`)} />
             </SearchIconWrapper>
             <StyledInputBase
               sx={{
@@ -497,14 +545,19 @@ export default function Header() {
           )}
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
+            {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+
+            <IconButton onClick={() => { setShowChat(true) }}
               size="large"
               aria-label="show 4 new mails"
               color="inherit"
             >
+
+
+
               <Badge badgeContent={1} color="error">
                 <svg
-                  onClick={() => navigate("/chat")}
+
                   rpl=""
                   fill="currentColor"
                   height="20"
@@ -517,7 +570,14 @@ export default function Header() {
                   <path d="M11.61 19.872a10.013 10.013 0 0 0 6.51-4.035A9.999 9.999 0 0 0 12.275.264c-1.28-.3-2.606-.345-3.903-.132a10.05 10.05 0 0 0-8.25 8.311 9.877 9.877 0 0 0 1.202 6.491l-1.24 4.078a.727.727 0 0 0 .178.721.72.72 0 0 0 .72.19l4.17-1.193A9.87 9.87 0 0 0 9.998 20c.54 0 1.079-.043 1.612-.128ZM1.558 18.458l1.118-3.69-.145-.24A8.647 8.647 0 0 1 1.36 8.634a8.778 8.778 0 0 1 7.21-7.27 8.765 8.765 0 0 1 8.916 3.995 8.748 8.748 0 0 1-2.849 12.09 8.763 8.763 0 0 1-3.22 1.188 8.68 8.68 0 0 1-5.862-1.118l-.232-.138-3.764 1.076ZM6.006 9a1.001 1.001 0 0 0-.708 1.707A1 1 0 1 0 6.006 9Zm4.002 0a1.001 1.001 0 0 0-.195 1.981 1 1 0 1 0 .195-1.98Zm4.003 0a1.001 1.001 0 1 0 0 2.003 1.001 1.001 0 0 0 0-2.003Z"></path>
                 </svg>
               </Badge>
+
+              {showChat && <Chat />}
+
+
             </IconButton>
+
+            {/* xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */}
+
             <IconButton
               size="large"
               aria-label="show 17 new notifications"
