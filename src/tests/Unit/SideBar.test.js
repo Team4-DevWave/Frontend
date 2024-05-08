be0c48368e3b5989b079277d23ed7769b5e8c31b
theHeliE <129796088+theHeliE@users.noreply.github.com>
2024-05-08 21:08:02 +0300

    unit tests

--- src/tests/Unit/SideBar.test.js
+++ src/tests/Unit/SideBar.test.js
@@ -1,101 +1,26 @@
 import React from "react";
-import { render, fireEvent, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Sidebar from "../../layouts/Sidebar";
+import { BrowserRouter as Router } from "react-router-dom";
 
-describe("Sidebar", () => {
-  test("renders Sidebar component without crashing", () => {
-    render(<Sidebar />);
-  });
+// Test the SideBar component
+describe("SideBar", () => {
+  it("renders the correct elements", () => {
+    // Render the SideBar component
+    render(
+      <Router>
+        <Sidebar />
+      </Router>
+    );
 
-  test("renders anchor tags", async () => {
-    render(<Sidebar />);
-    const links = await screen.findAllByRole("link");
-    expect(links.length).toBeGreaterThan(0);
-  });
-
-  test("renders details and summary", () => {
-    render(<Sidebar />);
-    const details = screen.getAllByRole("group");
-    expect(details.length).toBeGreaterThan(0);
-  });
-
-  test("toggles details when summary is clicked", () => {
-    render(<Sidebar />);
-    const groups = screen.getAllByRole("group");
-
-    // Check that the details are initially expanded
-    groups.forEach((group) => {
-      expect(group).toHaveAttribute("open");
-    });
-
-    // Click each summary
-    groups.forEach((group) => {
-      const { getByTestId } = within(group);
-      const summary = getByTestId("summary");
-      fireEvent.click(summary);
-    });
-
-    // Check that the details are collapsed after clicking the summary
-    groups.forEach((group) => {
-      expect(group).not.toHaveAttribute("open");
-    });
-  });
-
-  test("renders the correct text in the summary", () => {
-    render(<Sidebar />);
-    const summaries = screen.getAllByTestId("summary");
-    const expectedText = [
-      "MODERATION TOOLS",
-      "RECENT",
-      "COMMUNITIES",
-      "RESOURCES",
-    ];
-
-    summaries.forEach((summary, index) => {
-      expect(summary).toHaveTextContent(expectedText[index]);
-    });
-  });
-  test("renders the correct text in resources", () => {
-    render(<Sidebar />);
-    const expectedText = [
-      "About Threadit",
-      "Advertise",
-      "Help",
-      "Blog",
-      "Careers",
-      "Press",
-      "Communities",
-      "Best of Threadit",
-      "Topics",
-      "Content Policy",
-      "Privacy Policy",
-      "User Agreement",
-    ];
-
-    const resourcesList = screen.getByTestId("resources-list");
-    const resourcesItems = within(resourcesList).getAllByRole("listitem");
-
-    resourcesItems.forEach((item, index) => {
-      expect(item).toHaveTextContent(expectedText[index]);
-    });
-  });
-
-  test("renders imgs", () => {
-    render(<Sidebar />);
-    const imgs = screen.getAllByRole("img");
-    expect(imgs.length).toBeGreaterThan(0);
-  });
-
-  test("renders svgs", () => {
-    render(<Sidebar />);
-    const svgs = screen.getAllByRole("svg");
-    expect(svgs.length).toBeGreaterThan(0);
-  });
-
-  test("navigates to home page when home is clicked", () => {
-    render(<Sidebar />);
-    const home = screen.getByTestId("home-nav");
-    fireEvent.click(home);
-    expect(window.location.pathname).toBe("/");
+    // Assert that the component renders the expected elements
+    expect(screen.getByText("Home")).toBeInTheDocument();
+    expect(screen.getByText("Popular")).toBeInTheDocument();
+    expect(screen.getByText("All")).toBeInTheDocument();
+    expect(screen.getByText("t/Akira")).toBeInTheDocument();
+    expect(screen.getByText("COMMUNITIES")).toBeInTheDocument();
+    expect(screen.getByText("FAVORITES")).toBeInTheDocument();
+    expect(screen.getByText("RESOURCES")).toBeInTheDocument();
   });
 });
