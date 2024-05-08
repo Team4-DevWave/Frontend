b56d1bb96a97b4ffd8f77f522e92b40bb1898482
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-05-09 00:56:48 +0300

    Update RightSidebar.js

--- src/components/userProfile/RightSidebar.js
+++ src/components/userProfile/RightSidebar.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"; // Import Link from React Router
 import axios from "axios";
 import Cookies from "js-cookie";
 import { Button, Typography, Divider } from "@mui/material";
+import styles from "../../pages/Profile.module.css";
 
 const getTodayDate = () => {
   const today = new Date();
@@ -40,25 +41,25 @@ function RightSidebar({ postKarma, commentKarma, cakeDay, goldReceived }) {
   }, []);
 
   return (
-    <div className="right-sidebar">
-      <div className="user-info-container">
-        <div className="user-info">
+    <div className={styles.rightSidebar}>
+      <div className={styles.userInfoContainer}>
+        <div className={styles.userInfo}>
           <Typography variant="h5">{userName}</Typography>
           <Typography variant="body1">Post Karma: {postKarma}</Typography>
           <Typography variant="body1">Comment Karma: {commentKarma}</Typography>
         </div>
-        <div className="user-info">
+        <div className={styles.userInfo}>
           <Typography variant="body1">Cake Day: {cakeDay}</Typography>
           <Typography variant="body1">Gold Received: {goldReceived}</Typography>
         </div>
       </div>
       <Divider />
-      <div className="about">
+      <div className={styles.about}>
         <Typography variant="h5">About</Typography>
         <Typography variant="body2">{about}</Typography>
       </div>
       <Divider />
-      <div className="settings">
+      <div className={styles.settings}>
         <Link to="/settings">
           <Button variant="contained" color="primary">
             Edit Profile
@@ -66,7 +67,7 @@ function RightSidebar({ postKarma, commentKarma, cakeDay, goldReceived }) {
         </Link>
       </div>
       <Divider />
-      <div className="moderation">
+      <div className={styles.moderation}>
         <Typography variant="h5">Moderation</Typography>
         <Link to="https://www.reddit.com/user/Ok_Operation_7782/about/edit/moderation/">
           <Button variant="outlined">Mod Settings</Button>
@@ -74,7 +75,7 @@ function RightSidebar({ postKarma, commentKarma, cakeDay, goldReceived }) {
       </div>
 
       <Divider />
-      <div className="links">
+      <div className={styles.links}>
         <Typography variant="h5">Links</Typography>
         <SocialLinks />
       </div>
