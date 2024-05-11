811745566c98c14289d6198d093e132193d3737e
mohamed07abdelaziz <88625570+mohamed07abdelaziz@users.noreply.github.com>
2024-05-12 00:17:35 +0300

    tt

--- src/tests/Unit/Unread.test.js
+++ src/tests/Unit/Unread.test.js
@@ -1,14 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import axios from 'axios';
 import Unread from '../../components/messages/Unread.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
+global.IntersectionObserver = class IntersectionObserver {
+    constructor() {}
+  
+    disconnect() {}
+  
+    observe() {}
+  
+    takeRecords() {}
+  
+    unobserve() {}
+  };
+jest.mock('axios');
 
 describe('Unread component', () => {
-  test('renders without crashing', () => {
-    render(
+  test('marks a message as read', async () => {
+    const message = { _id: '1', from: { username: 'user1' }, to: { username: 'user2' }, subject: 'Test', message: 'Test message' };
+    axios.get.mockResolvedValueOnce({ data: { data: { messages: [message] } } });
+    axios.patch.mockResolvedValueOnce({ data: { data: { message: { ...message, read: true } } } });
+
+    const { getByText, findByText } = render(
+        <Router>
+          <Unread />
+        </Router>
+      );
+
+    const markUnreadButton = await findByText('Mark Unread');
+    fireEvent.click(markUnreadButton);
+
+    expect(axios.patch).toHaveBeenCalledWith(`http://localhost:8000/api/v1/messages/${message._id}/markread`, { read: true }, expect.anything());
+  });
+
+  test('deletes a message', async () => {
+    const message = { _id: '1', from: { username: 'user1' }, to: { username: 'user2' }, subject: 'Test', message: 'Test message' };
+    axios.get.mockResolvedValueOnce({ data: { data: { messages: [message] } } });
+    axios.delete.mockResolvedValueOnce({});
+  
+    const { getByText, findByText } = render(
       <Router>
         <Unread />
       </Router>
     );
-    // Add more assertions here
+  
+    const deleteButton = await findByText('Delete');
+    fireEvent.click(deleteButton);
+  
+    expect(axios.delete).toHaveBeenCalledWith(`http://localhost:8000/api/v1/messages/${message._id}/delete`, expect.anything());
   });
 });
